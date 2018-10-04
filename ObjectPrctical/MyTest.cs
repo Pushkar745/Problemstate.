0d01044cd03645dacd83d60c43dbcbf7a74d1647
Pushkar Sudhir Baviskar <b.pushkar745@gmail.com>
2018-10-04 23:06:09 +0530

    Designite problem statement.

---
+++ ObjectPrctical/MyTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ObjectPrctical
+{
+    [TestFixture]
+    class MyTest
+    {
+        [Test]
+        public void Readtext()
+        {
+       
+
+            string text1 = File.ReadAllText(@"E:\Designite\push.txt");
+            
+            string text = File.ReadAllText(@"E:\Designite\pushkar.txt");
+            Assert.IsFalse(string.IsNullOrEmpty(text1));
+            Assert.AreNotEqual(text1, text);
+            Assert.AreEqual(text, text1);
+
+
+
+        }
+        
+    }
+}
--- ObjectPrctical/Program.cs
+++ ObjectPrctical/Program.cs
@@ -1,15 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Globalization;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading;
+using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace ObjectPrctical
+class Program
 {
-    class Program
+public static void Main()
     {
-        static void Main(string[] args)
-        {
-        }
+        
+        Program p = new Program();
+        p.Readtext();
+       
     }
+
+
+    public  void Readtext()
+    {
+        try
+        {
+            
+            string text = File.ReadAllText(@"E:\Designite\pushkar.txt");
+            
+            string now = Convert.ToString(DateTime.UtcNow.ToShortDateString());
+            File.AppendAllText(@"E:\Designite\pushkar.txt", now);
+            Console.WriteLine(text);
+         
+            byte[] b = ASCIIEncoding.ASCII.GetBytes(text);
+            string encrypted = Convert.ToBase64String(b);
+            File.AppendAllText(@"E:\Designite\pushkar.txt", encrypted);
+            string Etext = encrypted;
+            File.WriteAllText(@"E:\Designite\push.txt", Etext);
+            string text1 = File.ReadAllText(@"E:\Designite\push.txt");
+            Console.WriteLine(text1);
+
+            Console.ReadKey();
+           
+        }
+            catch (Exception e)
+        {
+            Console.WriteLine("The file could not be read:");
+            Console.WriteLine(e.Message);
+
+        }
+            Console.ReadKey();
+    }
+   
 }
